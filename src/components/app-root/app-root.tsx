ca7be6f1695e3ceed0525817cc3ac67ac8535d16
Christian Cook <3473396+CookieCookson@users.noreply.github.com>
2024-01-29 18:08:28 +0000

    Add Slippy Map Tiles content

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -24,28 +24,29 @@ export class AppRoot {
           <cookie-heading>Creations</cookie-heading>
           <cookie-list>
             <cookie-list-item>
+              <cookie-link url="https://github.com/CookieCookson/slippy-tile-generator" external={true}>
+                Slippy Tile Generator
+              </cookie-link> - a JavaScript CLI for generating Slippy map tiles.
+            </cookie-list-item>
+            <cookie-list-item>
               <cookie-link url="https://www.xapijs.dev" external={true}>
                 xAPI.js: xAPI Wrapper Library
-              </cookie-link>
-              - a JavaScript library for elearning developers to communicate over the xAPI specification.
+              </cookie-link> - a JavaScript library for elearning developers to communicate over the xAPI specification.
             </cookie-list-item>
             <cookie-list-item>
               <cookie-link url="https://www.xapijs.dev/cmi5-profile-library/introduction" external={true}>
                 xAPI.js: cmi5 Profile Library
-              </cookie-link>
-              - a JavaScript library for elearning developers to communicate over the cmi5 specification.
+              </cookie-link> - a JavaScript library for elearning developers to communicate over the cmi5 specification.
             </cookie-list-item>
             <cookie-list-item>
               <cookie-link url="https://codepen.io/cookiecookson/pen/KKMMYjJ" external={true}>
                 Star Wars: Squadrons Loader
-              </cookie-link>
-              - a small side-project challenge to replicate the loader from the game using CSS.
+              </cookie-link> - a small side-project challenge to replicate the loader from the game using CSS.
             </cookie-list-item>
             <cookie-list-item>
               <cookie-link url="https://gist.github.com/CookieCookson/5eb4f0b5e0ba82b28d2a3fd7e157f4bd" external={true}>
                 Cookie's Paella 🍪🥘
-              </cookie-link>
-              - A favoured paella recipe that I have been cooking for my friends for many years.
+              </cookie-link> - A favoured paella recipe that I have been cooking for my friends for many years.
             </cookie-list-item>
           </cookie-list>
         </cookie-section>
